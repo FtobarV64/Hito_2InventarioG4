60d77664b8052b942c6519155a9e35a6ab524dcd
FtobarV64 <ftobarv@gmail.com>
2022-05-21 20:13:21 -0400

    adding kaminari and create seed commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+  
+  50.times do |i|
+    Product.create(code: "Cod_#{i}", description: "Producto #{i}", salesUnit: "un")
+  end
